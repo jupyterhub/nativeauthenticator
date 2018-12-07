2cb01f089086bd31da590b98460f1a52b8172e6a
Leticia Portella <leportella@gmail.com>
2018-12-07 12:38:46 -0200

    Add simples signin handler

--- nativeauthenticator/nativeauthenticator.py
+++ nativeauthenticator/nativeauthenticator.py
@@ -1,6 +1,13 @@
 from jupyterhub.auth import Authenticator
+from jupyterhub.handlers import BaseHandler
 
 from tornado import gen
+
+
+class SignInHandler(BaseHandler):
+
+    async def get(self):
+        pass
 
 
 class NativeAuthenticator(Authenticator):
@@ -8,3 +15,9 @@ class NativeAuthenticator(Authenticator):
     @gen.coroutine
     def authenticate(self, handler, data):
         return data['username']
+
+    def get_handlers(self, app):
+        native_handlers = [
+            (r'/signin', SignInHandler)
+        ]
+        return super().get_handlers(app) + native_handlers
