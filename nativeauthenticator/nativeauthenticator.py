3ee8a5110c6c8fa418e340aba4a2328e61b104e7
Leticia Portella <leportella@gmail.com>
2018-12-07 12:47:42 -0200

    Add template to SignIn Handler

--- nativeauthenticator/nativeauthenticator.py
+++ nativeauthenticator/nativeauthenticator.py
@@ -1,13 +1,35 @@
+import os
+from jinja2 import ChoiceLoader, FileSystemLoader
 from jupyterhub.auth import Authenticator
 from jupyterhub.handlers import BaseHandler
 
 from tornado import gen
 
 
+TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
+
+
 class SignInHandler(BaseHandler):
+    """Render the sign in page."""
+
+    def __init__(self, *args, **kwargs):
+        self._loaded = False
+        super().__init__(*args, **kwargs)
+
+    def _register_template_path(self):
+        if self._loaded:
+            return
+        self.log.debug('Adding %s to template path', TEMPLATE_DIR)
+        loader = FileSystemLoader([TEMPLATE_DIR])
+        env = self.settings['jinja2_env']
+        previous_loader = env.loader
+        env.loader = ChoiceLoader([previous_loader, loader])
+        self._loaded = True
 
     async def get(self):
-        pass
+        self._register_template_path()
+        html = self.render_template('signin.html')
+        self.finish(html)
 
 
 class NativeAuthenticator(Authenticator):
