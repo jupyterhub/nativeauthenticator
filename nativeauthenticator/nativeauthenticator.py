a06c2c177ae72b5fc3e3f42d41835bf0773ff703
Leticia Portella <leportella@gmail.com>
2019-01-02 11:40:20 +0000

    Add user creation after signup form being filled

--- nativeauthenticator/nativeauthenticator.py
+++ nativeauthenticator/nativeauthenticator.py
@@ -1,5 +1,6 @@
 import os
 from jinja2 import ChoiceLoader, FileSystemLoader
+from jupyterhub import orm
 from jupyterhub.auth import Authenticator
 from jupyterhub.handlers import BaseHandler
 
@@ -32,9 +33,11 @@ class SignUpHandler(BaseHandler):
         self.finish(html)
 
     async def post(self):
+        username = self.get_body_argument('username', strip=False)
+        user = self.authenticator.get_or_create_user(username)
         html = self.render_template(
                  'signup.html',
-                 result=True,
+                 result=bool(user),
                  result_message='Your information have been sent to the admin',
         )
         self.finish(html)
@@ -45,6 +48,13 @@ class NativeAuthenticator(Authenticator):
     @gen.coroutine
     def authenticate(self, handler, data):
         return data['username']
+
+    def get_or_create_user(self, username):
+        user = orm.User.find(self.db, username)
+        if not user:
+            user = orm.User(name=username, admin=False)
+            self.db.add(user)
+        return user
 
     def get_handlers(self, app):
         native_handlers = [
