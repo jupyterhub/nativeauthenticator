74a17c9fe8363e8dc8e4bbd49ca54bc08e16fddf
Leticia Portella <leportella@gmail.com>
2019-01-02 17:51:31 +0000

    Create new table users_info on Auth class __init__

--- nativeauthenticator/nativeauthenticator.py
+++ nativeauthenticator/nativeauthenticator.py
@@ -1,21 +1,30 @@
-from jupyterhub import orm
+from jupyterhub.orm import User
 from jupyterhub.auth import Authenticator
 
+from sqlalchemy import inspect
 from tornado import gen
 
 from .handlers import SignUpHandler
+from .orm import UserInfo
 
 
 class NativeAuthenticator(Authenticator):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        inspector = inspect(self.db.bind)
+        if 'users_info' not in inspector.get_table_names():
+            UserInfo.__table__.create(self.db.bind)
 
     @gen.coroutine
     def authenticate(self, handler, data):
         return data['username']
 
     def get_or_create_user(self, username):
-        user = orm.User.find(self.db, username)
+        user = User.find(self.db, username)
         if not user:
-            user = orm.User(name=username, admin=False)
+            user = User(name=username, admin=False)
             self.db.add(user)
         return user
 
