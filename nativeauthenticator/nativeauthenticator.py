8debaebd08d1de3b10b8962788b8273f0ece24a8
Leticia Portella <leportella@gmail.com>
2019-01-10 10:56:31 +0000

    Add relationship between User and UserInfo

--- nativeauthenticator/nativeauthenticator.py
+++ nativeauthenticator/nativeauthenticator.py
@@ -3,6 +3,7 @@ from jupyterhub.orm import User
 from jupyterhub.auth import Authenticator
 
 from sqlalchemy import inspect
+from sqlalchemy.orm import relationship
 from tornado import gen
 
 from .handlers import SignUpHandler
@@ -11,11 +12,16 @@ from .orm import UserInfo
 
 class NativeAuthenticator(Authenticator):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, add_new_table=True, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        if add_new_table:
+            self.add_new_table()
+
+    def add_new_table(self):
         inspector = inspect(self.db.bind)
         if 'users_info' not in inspector.get_table_names():
+            User.info = relationship(UserInfo, backref='users')
             UserInfo.__table__.create(self.db.bind)
 
     @gen.coroutine
@@ -31,7 +37,7 @@ class NativeAuthenticator(Authenticator):
             self.db.add(user)
 
         encoded_pw = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-        user_info = UserInfo(username=username, password=encoded_pw)
+        user_info = UserInfo(user=user, username=username, password=encoded_pw)
         self.db.add(user_info)
         return user
 
