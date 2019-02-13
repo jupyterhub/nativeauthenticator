2106313ce763c33dadb9a6d6e6e05fa1effb7df6
00Kai0 <sunkaihuisos@gmail.com>
2019-02-13 11:01:36 +0800

    add orm test

--- nativeauthenticator/tests/test_orm.py
+++ nativeauthenticator/tests/test_orm.py
@@ -1,5 +1,6 @@
 import pytest
 from jupyterhub.tests.mocking import MockHub
+from sqlalchemy.exc import StatementError
 
 from ..orm import UserInfo
 
@@ -19,10 +20,17 @@ def app():
 @pytest.mark.parametrize('email', ['john', 'john@john'])
 def test_validate_method_wrong_email(email, tmpdir, app):
     with pytest.raises(AssertionError):
-        UserInfo(username='john', password='pwd', email=email)
+        UserInfo(username='john', password=b'pwd', email=email)
 
 
 def test_validate_method_correct_email(tmpdir, app):
-    user = UserInfo(username='john', password='pwd', email='john@john.com')
+    user = UserInfo(username='john', password=b'pwd', email='john@john.com')
     app.db.add(user)
     assert UserInfo.find(app.db, 'john')
+
+
+def test_wrong_pwd_type(tmpdir, app):
+    with pytest.raises(StatementError):
+        user = UserInfo(username='john', password='pwd', email='john@john.com')
+        app.db.add(user)
+        UserInfo.find(app.db, 'john')
