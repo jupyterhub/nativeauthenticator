1477840c9e120b3e6ce27222ffc200489a55c634
martin donadieu <martindonadieu@gmail.com>
2019-10-04 15:28:04 +0100

    add back missing functions

--- nativeauthenticator/handlers.py
+++ nativeauthenticator/handlers.py
@@ -28,6 +28,75 @@ class LocalBase(BaseHandler):
         env.loader = ChoiceLoader([previous_loader, loader])
         self._loaded = True
 
+
+class SignUpHandler(LocalBase):
+    """Render the sign in page."""
+    async def get(self):
+        self._register_template_path()
+        html = self.render_template(
+            'signup.html',
+            ask_email=self.authenticator.ask_email_on_signup,
+            two_factor_auth=self.authenticator.allow_2fa,
+        )
+        self.finish(html)
+
+    def get_result_message(self, user):
+        alert = 'alert-info'
+        message = 'Your information have been sent to the admin'
+
+        if self.authenticator.open_signup:
+            alert = 'alert-success'
+            message = ('The signup was successful. You can now go to '
+                       'home page and log in the system')
+        else:
+            alert = 'alert-danger'
+            message = ('Signup not allowed.'
+                       'ask an Cashstory Admin to get access')
+            return alert, message
+        if not user:
+            alert = 'alert-danger'
+            pw_len = self.authenticator.minimum_password_length
+
+            if pw_len:
+                message = ("Something went wrong. Be sure your password has "
+                           "at least {} characters, doesn't have spaces or "
+                           "commas and is not too common.").format(pw_len)
+
+            else:
+                message = ("Something went wrong. Be sure your password "
+                           " doesn't have spaces or commas and is not too "
+                           "common.")
+
+        return alert, message
+
+    async def post(self):
+        user_info = {
+            'username': self.get_body_argument('username', strip=False),
+            'pw': self.get_body_argument('pw', strip=False),
+            'email': self.get_body_argument('email', '', strip=False),
+            'has_2fa': bool(self.get_body_argument('2fa', '', strip=False)),
+        }
+        user = self.authenticator.get_or_create_user(**user_info)
+
+        alert, message = self.get_result_message(user)
+
+        otp_secret, user_2fa = '', ''
+        if user:
+            otp_secret = user.otp_secret
+            user_2fa = user.has_2fa
+
+        html = self.render_template(
+            'signup.html',
+            ask_email=self.authenticator.ask_email_on_signup,
+            result_message=message,
+            alert=alert,
+            two_factor_auth=self.authenticator.allow_2fa,
+            two_factor_auth_user=user_2fa,
+            two_factor_auth_value=otp_secret,
+        )
+        self.finish(html)
+
+
 class AuthorizationHandler(LocalBase):
     """Render the sign in page."""
     @admin_only
@@ -46,6 +115,33 @@ class ChangeAuthorizationHandler(LocalBase):
     async def get(self, slug):
         UserInfo.change_authorization(self.db, slug)
         self.redirect('/authorize')
+
+
+class ChangePasswordHandler(LocalBase):
+    """Render the reset password page."""
+
+    @web.authenticated
+    async def get(self):
+        self._register_template_path()
+        html = self.render_template('change-password.html')
+        self.finish(html)
+
+    @web.authenticated
+    async def post(self):
+        user = await self.get_current_user()
+        new_password = self.get_body_argument('password', strip=False)
+        message = ''
+        if self.authenticator.open_change_password:
+            message = 'Your password has been changed successfully'
+            self.authenticator.change_password(user.name, new_password)
+        else:
+            message = 'You can\'t change your password, ask an Admin'
+        html = self.render_template(
+            'change-password.html',
+            result_message=message,
+        )
+        self.finish(html)
+
 
 class LoginHandler(LoginHandler, LocalBase):
 
