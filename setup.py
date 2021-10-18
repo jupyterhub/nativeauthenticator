556e149759f44f44dfe7cabfc39b20d76517f253
Jonas Betzendahl <jonas.betzendahl@posteo.de>
2021-10-18 22:33:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='jupyterhub-nativeauthenticator',
-    version='0.0.7',
+    version='1.0.0',
     description='JupyterHub Native Authenticator',
     long_description=long_description,
     long_description_content_type="text/markdown",
