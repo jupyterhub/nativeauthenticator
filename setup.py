c1dc160e8cc638bf17837601b5067a9a014b5d1a
Jonas Betzendahl <jonas.betzendahl@posteo.de>
2021-10-19 14:04:08 +0200

    Bump version to 1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='jupyterhub-nativeauthenticator',
-    version='1.0.1',
+    version='1.0.2',
     description='JupyterHub Native Authenticator',
     long_description=long_description,
     long_description_content_type="text/markdown",
