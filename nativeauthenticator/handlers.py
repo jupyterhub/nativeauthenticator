1a77815d929c8082ee9f85a2f398c0515159141a
Jonas Betzendahl <jonas.betzendahl@fau.de>
2021-10-30 20:57:11 +0200

    change password change handlers to respect criteria

--- nativeauthenticator/handlers.py
+++ nativeauthenticator/handlers.py
@@ -248,12 +248,23 @@ class ChangePasswordHandler(LocalBase):
     async def post(self):
         user = await self.get_current_user()
         new_password = self.get_body_argument('password', strip=False)
-        self.authenticator.change_password(user.name, new_password)
+        success = self.authenticator.change_password(user.name, new_password)
+
+        if success:
+            alert = 'alert-success'
+            msg = 'Your password has been changed successfully!'
+        else:
+            alert = 'alert-danger'
+            pw_len = self.authenticator.minimum_password_length
+            msg = ('Something went wrong! Be sure your new '
+                   'password has at least {} characters and is '
+                   'not too common.').format(pw_len)
 
         html = await self.render_template(
             'change-password.html',
             user_name=user.name,
-            result_message='Your password has been changed successfully',
+            result_message=msg
+            alert=alert
         )
         self.finish(html)
 
@@ -274,13 +285,24 @@ class ChangePasswordAdminHandler(LocalBase):
     @admin_users_scope
     async def post(self, user_name):
         new_password = self.get_body_argument('password', strip=False)
-        self.authenticator.change_password(user_name, new_password)
+        success = self.authenticator.change_password(user.name, new_password)
 
-        message_template = 'The password for {} has been changed successfully'
+        if success:
+            alert = 'alert-success'
+            msg = ('The password for {} has been changed '
+                   'successfully').format(user_name)
+        else:
+            alert = 'alert-danger'
+            pw_len = self.authenticator.minimum_password_length
+            msg = ('Something went wrong! Be sure the new password '
+                   'for {} has at least {} characters and is '
+                   'not too common.').format(user_name, pw_len)
+
         html = await self.render_template(
             'change-password.html',
-            user_name=user_name,
-            result_message=message_template.format(user_name),
+            user_name=user.name,
+            result_message=msg
+            alert=alert
         )
         self.finish(html)
 
