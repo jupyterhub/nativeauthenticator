d4ce40f1c3b1a20d6381fb7e36c21bcf7b4f8ab9
Scot_Survivor <unconfigured@null.spigotmc.org>
2022-12-21 13:37:57 +0000

    [FIX] Timing Attack Vulnerability
    + In the method "is_valid_password", the method was using `==` operator to compare hashes. This is vulnerable to timing attacks & has now been patched by using the secrets.compare_digest method.

--- nativeauthenticator/orm.py
+++ nativeauthenticator/orm.py
@@ -1,7 +1,7 @@
 import base64
 import os
 import re
-
+import secrets
 import bcrypt
 import onetimepass
 from jupyterhub.orm import Base
@@ -98,7 +98,7 @@ class UserInfo(Base):
         passing self.password back to bcrypt.hashpw(...) as a salt, it is smart
         enough to extract and use only the salt that was originally used.
         """
-        return self.password == bcrypt.hashpw(password.encode(), self.password)
+        return secrets.compare_digest(self.password, bcrypt.hashpw(password.encode(), self.password))
 
     @validates("email")
     def validate_email(self, key, address):
