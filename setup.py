725e179db2332789c55690753f8d8d984beca253
Erik Sundell <erik@sundellopensource.se>
2024-09-17 15:55:58 +0200

    Bump to 1.3.1.dev

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as fh:
 
 setup(
     name="jupyterhub-nativeauthenticator",
-    version="1.3.0",
+    version="1.3.1.dev",
     description="JupyterHub Native Authenticator",
     long_description=long_description,
     long_description_content_type="text/markdown",
